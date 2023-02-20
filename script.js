82e5b785e1b842ac61bd93b6467485b66a5226de
JustinaRockmantic <117934257+JustinaRockmantic@users.noreply.github.com>
2023-02-20 18:21:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+/* ------------------------------ TASK 7 -----------------------------------
+Turimas "audi" objektas.
+
+Parašykite funkciją showObjectKeys, kuri kaip argumentą priims objektą 
+ir grąžins visus jo "key" masyve.
+-------------------------------------------------------------------------- */
+const audi = {
+  make: "audi",
+  model: "A6",
+  year: 2005,
+  color: "white",
+};
+
+console.log(Object.keys(audi));
+
+// arba
+
+const showObjectKeys = Object.keys(audi);
+console.log(showObjectKeys);
